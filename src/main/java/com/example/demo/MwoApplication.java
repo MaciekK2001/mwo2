eeab844fcc0182a94016aef4b536e6d939963cbb
Maciej Kwiatkowski <maciek-kwiatkowski@o2.pl>
2023-12-01 00:00:03 +0100

    first commit

---
+++ src/main/java/com/example/demo/MwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwoApplication.class, args);
+	}
+
+}
